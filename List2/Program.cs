6f644a456b48a69be272a6e93e7d9329888a0461
Samir Crljenkovic <103046492@student.swin.edu.au>
2020-04-12 21:52:39 +1000

    asd

--- List2/Program.cs
+++ List2/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace list2
 {
     class Program
     {
         static void Main(string[] args)
+    
         {
-            Console.WriteLine("Hello World!");
-        }
+           /*
+            List<int> numlist = new List<int>();
+            
+            Console.WriteLine("Enter same number 5 times");
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i< numlist.Count; i++)
+            {
+                list.Add(int.Parse(Console.WriteLine()));
+            }
+            */
+
+
+            List<int> list = new List<int>();
+
+            Console.WriteLine("Enter numbers 1-5 until dupe detected");
+            
+            for(int i=0; i<5; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+
+            }
+
+            
+
+            if (list.Contains(dupe))
+            {
+            int dupe = int.Parse(Console.ReadLine());
+            
+            for (int i=0; i < dupe.Count; i++)
+            {
+                System.Console.WriteLine(dupe[i]);
+            }
+            }   
+        }   
     }
 }
